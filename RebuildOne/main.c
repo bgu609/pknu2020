84a001077d74f28c98781ad618a8e184b002340e
bgu <giung609@naver.com>
2020-02-03 10:38:12 +0900

    4장 완료

--- RebuildOne/main.c
+++ RebuildOne/main.c
@@ -13,6 +13,7 @@
 // 메인함수
 int main(void) 
 {
+    /*
     int a = 10, b = 20;
     int res = 2;
 
@@ -21,6 +22,24 @@ int main(void)
 
     printf("a=%d, b=%d\n", a, b);
     printf("res=%d\n", res);
+    */
+
+    /*
+    int a = 10, b = 20, res;
+
+    res = (a > b) ? a : b;
+    printf("큰 값 : %d\n", res);
+    */
+
+    int a = 10;
+    int b = 12;
+
+    printf("a&b : %d\n", a & b);
+    printf("a^b : %d\n", a ^ b);
+    printf("a|b : %d\n", a | b);
+    printf("~a : %d\n", ~a);
+    printf("a<<1 : %d\n", a << 1);
+    printf("a>>2 : %d\n", a >> 2);
 
 	system("pause");
 	return EXIT_SUCCESS;
