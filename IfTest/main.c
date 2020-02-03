5b602f17c69092d98451646ed8a2eb68e8d90bc0
bgu <giung609@naver.com>
2020-02-03 11:01:59 +0900

    5장

---
+++ IfTest/main.c
@@ -0,0 +1,32 @@
+/*
+  filename - main.c
+  version - 1.0
+  description - 기본 메인 함수 / 선택문 작업
+  --------------------------------------------------------------------------------
+  first created - 2020.02.01
+  writer - B G U.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// 메인함수
+int main(void) 
+{
+    int a = 10;
+    int b = 0;
+
+    if (a >= 0)
+    {
+        a = 1;
+    }
+    else
+    {
+        a = -1;
+    }
+
+	printf("a : %d\n",a);
+    
+	system("pause");
+	return EXIT_SUCCESS;
+}
