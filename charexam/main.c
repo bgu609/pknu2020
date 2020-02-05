fb07e5a34fd156d2f8e3309f1f961100a2320d06
bgu <giung609@naver.com>
2020-02-05 09:49:07 +0900

    12장

--- charexam/main.c
+++ charexam/main.c
@@ -3,8 +3,8 @@
   version - 1.0
   description - 기본 메인 함수
   --------------------------------------------------------------------------------
-  first created - 2020.02.01
-  writer - Hugo MG Sung.
+  first created - 2020.02.05
+  writer - BGU.
 */
 
 #include <stdio.h>
@@ -13,8 +13,12 @@
 // 메인함수
 int main(void) 
 {
-	printf("Hello World!\n");
-    // type here.
+    char* dessert = "apple";
+
+    printf("오늘 후식은 %s입니다.\n", dessert);
+    dessert = "banana";
+    printf("내일 후식은 %s입니다.\n", dessert);
+    
 	system("pause");
 	return EXIT_SUCCESS;
 }
