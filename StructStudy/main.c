92368335ef5c71840c17c34f585a7275d24d42fb
bgu <giung609@naver.com>
2020-02-07 15:30:50 +0900

    17-1완료

--- StructStudy/main.c
+++ StructStudy/main.c
@@ -16,17 +16,105 @@
 
 struct profile
 {
-    char* name;
     int age;
     double height;
-    char* intro;
 };
+
+struct student
+{
+    struct profile pf;
+    int id;
+    double grade;
+};
+
+struct grade
+{
+    int id;
+    char name[20];
+    double grade;
+};
+
+struct vision
+{
+    double left;
+    double right;
+};
+
+struct vision exchange(struct vision robot);
+//void strt_grade();
+//void strt_student();
 
 // 메인함수
 int main(void)
 {
+    struct vision robot;
+
+    printf("시력 입력 : ");
+    scanf("%lf%lf", &(robot.left), &(robot.right));
+    printf("\n처음 시력 : %.1lf %.1lf\n", robot.left, robot.right);
+    robot = exchange(robot);
+    printf("바뀐 시력 : %.1lf %.1lf\n", robot.left, robot.right);
+
+    //strt_grade();
+    //strt_student();
+    //strt_profile();
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
+
+struct vision exchange(struct vision robot)
+{
+    double temp;
+
+    temp = robot.left;
+    robot.left = robot.right;
+    robot.right = temp;
+
+    return robot;
+}
+
+void strt_grade(void)
+{
+    struct grade s1 = { 315,"홍길동",2.4 }, s2 = { 316,"이순신",3.7 }, s3 = { 317,"세종대왕",4.4 };
+
+    struct grade max;
+
+    max = s1;
+    if (s2.grade > max.grade)
+    {
+        max = s2;
+    }
+    if (s3.grade > max.grade)
+    {
+        max = s3;
+    }
+
+    printf("학번 : %d\n", max.id);
+    printf("이름 : %s\n", max.name);
+    printf("학점 : %.1lf\n", max.grade);
+}
+
+void strt_student(void)
+{
+    struct student yuni;
+
+    yuni.pf.age = 17;
+    yuni.pf.height = 164.5;
+    yuni.id = 315;
+    yuni.grade = 4.3;
+
+    printf("     나이\t: %d\n", yuni.pf.age);
+    printf("      키\t: %.1lf\n", yuni.pf.height);
+    printf("     학번\t: %d\n", yuni.id);
+    printf("     학점\t: %.1lf\n", yuni.grade);
+}
+
+/*
+void strt_profile(void)
+{
     struct profile yuni;
-    
+
     yuni.name = (char*)malloc(20);
     printf("이름 : ");
     gets(yuni.name);
@@ -46,7 +134,5 @@ int main(void)
     printf("   자기소개\t: %s\n", yuni.intro);
     free(yuni.name);
     free(yuni.intro);
-
-    system("pause");
-    return EXIT_SUCCESS;
 }
+*/
