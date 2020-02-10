5437004f08233a55c614fc53bf97ecf6184a250a
bgu <giung609@naver.com>
2020-02-10 11:50:53 +0900

    18장

---
+++ FileStudy/main.c
@@ -0,0 +1,104 @@
+/*
+  filename - main.c
+  version - 1.0
+  description - 기본 메인 함수
+  --------------------------------------------------------------------------------
+  first created - 2020.02.10
+  writer - BGU.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//int file_open();
+//int file_write();
+int std_inout();
+
+// 메인함수
+int main(void)
+{
+    int open= std_inout();
+
+    system("pause");
+    return EXIT_SUCCESS;
+}
+
+int file_open(void)
+{
+    FILE* fp;
+    int ch;
+
+    fp = fopen("D:\\documents\\target1.txt", "r");
+
+    if (fp == NULL)
+    {
+        printf("파일이 열리지 않았습니다.\n\n");
+        return 1;
+    }
+
+    printf("파일이 열렸습니다.\n\n");
+    
+    while (1)
+    {
+        ch = fgetc(fp);
+        if (ch == EOF)
+        {
+            printf("\n\n");
+            break;
+        }
+        putchar(ch);
+    }
+    fclose(fp);
+
+    return 0;
+}
+
+int file_write(void)
+{
+    FILE* fp;
+    char str[] = "banana";
+    int i;
+
+    fp = fopen("D:\\documents\\banana.txt", "w");
+
+    if (fp == NULL)
+    {
+        printf("파일을 만들지 못했습니다.\n\n");
+        return 1;
+    }
+
+    i = 0;
+
+    while (str[i]!='\0')
+    {
+        fputc(str[i], fp);
+        i++;
+    }
+    fputc('\n', fp);
+    fclose(fp);
+
+    return 0;
+}
+
+int std_inout(void)
+{
+    int ch;
+
+    while (1)
+    {
+        //ch = getchar();
+        ch = fgetc(stdin);
+
+        if (ch == EOF)
+        {
+            break;
+        }
+
+        //putchar(ch);
+        fputc(ch, stdout);
+    }
+    return 0;
+}
