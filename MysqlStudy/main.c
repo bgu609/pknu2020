7541d61d38d2e2c37d9876e17faf9c969d9aed8b
bgu <giung609@naver.com>
2020-02-12 09:27:55 +0900

    sql

---
+++ MysqlStudy/main.c
@@ -0,0 +1,19 @@
+/*
+  filename - main.c
+  version - 1.0
+  description - 기본 메인 함수
+  --------------------------------------------------------------------------------
+  first created - 2020.02.12
+  writer - BGW.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 메인함수
+int main(void) 
+{
+	system("pause");
+	return EXIT_SUCCESS;
+}
